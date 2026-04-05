45cf703ca215b93f5b65d6d714048705d0d5f2e6
MerCuJerry <mercujerry@gmail.com>
2026-04-06 00:59:08 +0800

    migrate to alconna

--- nonebot_plugin_batitle/__init__.py
+++ nonebot_plugin_batitle/__init__.py
@@ -1,50 +1,46 @@
-from nonebot import on_startswith, require
+from nonebot import require
 from nonebot.plugin import PluginMetadata, inherit_supported_adapters
-from nonebot.params import EventPlainText, Startswith
-from nonebot.matcher import Matcher
 from .draw import draw_pic
 from io import BytesIO
-require("nonebot_plugin_saa")
-from nonebot_plugin_saa import MessageFactory, Image
+from arclet.alconna import Alconna, CommandMeta, Args
+from nepattern import AnyString
+require("nonebot_plugin_alconna")
+from nonebot_plugin_alconna import on_alconna, Match, AlconnaMatch, AlconnaMatcher  # noqa: E402
+from nonebot_plugin_alconna.uniseg import UniMessage, Image, Reply, get_message_id  # noqa: E402
 
 
-__version__ = "0.1.7"
+__version__ = "0.2.0"
 __plugin_meta__ = PluginMetadata(
     name="BlueArchive Title Generator",
     description="碧蓝档案式标题生成器",
-    usage="batitle 前|后",
+    usage="batitle 前 后",
     type="application",
     homepage="https://github.com/MerCuJerry/nonebot-plugin-batitle",
     config=None,
-    supported_adapters=inherit_supported_adapters("nonebot_plugin_saa"),
+    supported_adapters=inherit_supported_adapters("nonebot_plugin_alconna"),
     extra={
         "version": __version__,
         "author": "MerCuJerry <mercujerry@gmail.com>",
     },
 )
 
-batitle = ("batitle ", "ba标题 ")
-batitle_matcher = on_startswith(msg=batitle, block=True, priority=5)
-
+batitle = Alconna("batitle", Args["upper", AnyString]["lower", AnyString], meta=CommandMeta(description="生成一个碧蓝档案式的标题"))
+batitle.shortcut("ba标题", {"command": "batitle {*}"})
+batitle_matcher = on_alconna(batitle, block=True, priority=5)
 
 @batitle_matcher.handle()
 async def _handler(
-    matcher: Matcher, str_match: str = Startswith(), key: str = EventPlainText()
-):
+    matcher: AlconnaMatcher, 
+    upper: Match[str] = AlconnaMatch("upper"),
+    lower: Match[str] = AlconnaMatch("lower")):
     try:
-        keyword: str = key.replace(str_match, "")
-        if "｜" in keyword:
-            keyword = keyword.replace("｜", "|")
-        upper = keyword.split("|")[0].strip()
-        downer = keyword.split("|")[1].strip()
-        img_raw = draw_pic(front=upper, back=downer)
+        img_raw = draw_pic(front=upper.result, back=lower.result)
         img = BytesIO()
         img_raw.save(img, format="png")
         img_bytes: bytes = img.getvalue()
         img_raw.close()
         img.close()
-        await MessageFactory(Image(img_bytes)).send()
-        await matcher.finish()
+        await matcher.finish(await UniMessage([Reply(get_message_id()), Image(raw=img_bytes)]).export())
 
     except OSError:
         await matcher.finish("生成失败……请检查字体文件设置是否正确")
